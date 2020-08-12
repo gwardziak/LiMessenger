d2d85625b4ccde936085583ae2a9d77a478e6826
jaca <jaca@gmail.com>
2020-08-12 12:53:41 +0200

    init project

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import Express from "express";
+import graphqlHTTP from "express-graphql";
+import expressPlayground from "graphql-playground-middleware-express";
+import "reflect-metadata";
+import { buildSchema, useContainer } from "type-graphql";
+import Container from "typedi";
+import { createConnection } from "typeorm";
+
+useContainer(Container);
+
+const main = async () => {
+  await createConnection();
+
+  const schema = await buildSchema({
+    resolvers: [],
+    //validation - true when validating anything
+    validate: false,
+  });
+
+  const app = Express();
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+    })
+  );
+
+  app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+  app.listen(process.env.PORT || 4000);
+
+  console.log(
+    `Running a GraphQL API server at http://localhost:${
+      process.env.PORT || 4000
+    }/playground`
+  );
+};
+
+main().catch((err) => console.error(err));
