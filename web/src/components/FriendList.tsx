71828b8417e2900c459f8bc469c7d6788151d05a
jaca <jaca@gmail.com>
2020-09-28 13:12:55 +0200

    split friend list to separate file

---
+++ web/src/components/FriendList.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "../ui/Avatar";
+import { mediaQuery } from "../utils/css/cssMedia";
+
+export const FriendList = () => {
+  const users = [1, 2, 3, 4, 5, 5, 6];
+
+  return (
+    <>
+      <List>
+        <ItemSelected>
+          <AvatarContainer>
+            <Avatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_ohc=jpVmHYL2xgEAX_eH_Qn&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=e5c68a6969033f02f9d8f25d6a3d5560&oe=5F98D99E" />
+          </AvatarContainer>
+          <LastMessageContainer>
+            <Username>Adam Galon </Username>
+            <Message>User: Lorem lipsum · Dziś</Message>
+          </LastMessageContainer>
+        </ItemSelected>
+        {users.map(() => (
+          <Item>
+            <AvatarContainer>
+              <Avatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_ohc=jpVmHYL2xgEAX_eH_Qn&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=e5c68a6969033f02f9d8f25d6a3d5560&oe=5F98D99E" />
+            </AvatarContainer>
+            <LastMessageContainer>
+              <Username>Adam Galon </Username>
+              <Message>User: Lorem lipsum · Dziś</Message>
+            </LastMessageContainer>
+          </Item>
+        ))}
+      </List>
+    </>
+  );
+};
+
+const List = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  font-size: 13px;
+`;
+
+const Item = styled.li`
+  display: flex;
+  height: 64px;
+  position: relative;
+  padding: 0 8px;
+  margin: 0 8px;
+`;
+
+const ItemSelected = styled.li`
+  display: flex;
+  height: 64px;
+  position: relative;
+  padding: 0 8px;
+  background-color: rgba(0, 0, 0, 0.05);
+  margin: 0 8px;
+  border-radius: 10px;
+`;
+
+const AvatarContainer = styled.div`
+  margin-right: 12px;
+  display: flex;
+  align-items: center;
+`;
+
+const LastMessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0px;
+  justify-content: center;
+  padding: 0 16px 0 0;
+  font-weight: 400;
+
+  @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md}, {
+    display: none;
+  }
+`;
+
+const Username = styled.div`
+  font-size: 15px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  line-height: 1.4;
+`;
+
+const Message = styled.div`
+  font-size: 13px;
+  margin-right: 12px;
+  color: rgba(153, 153, 153, 1);
+`;
--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { FriendList } from "../components/FriendList";
+import { Camcorder } from "../Icons/Camcorder";
+import { Compose } from "../Icons/Compose";
+import { CreateNewRoom } from "../Icons/CreateNewRoom";
+import { InfoCircle } from "../Icons/InfoCircle";
+import { Phone } from "../Icons/Phone";
+import { Settings } from "../Icons/Settings";
+import { Avatar } from "../ui/Avatar";
 import { mediaQuery } from "../utils/css/cssMedia";
 
 export const Main = () => {
@@ -8,47 +16,102 @@ export const Main = () => {
   return (
     <Container>
       <LeftContainer>
-        <Nav>
-          <AvaContainer>
-            <Ava src="https://scontent-frx5-1.xx.fbcdn.net/v/t31.0-1/cp0/c0.23.60.60a/p60x60/20507795_749401998564414_4605004011507775542_o.jpg?_nc_cat=110&_nc_sid=dbb9e7&_nc_ohc=Af0ZRd917-sAX8OykP7&_nc_ht=scontent-frx5-1.xx&oh=0fac05dc8c491e475a491a3afcdbb493&oe=5F8D7C12"></Ava>
-          </AvaContainer>
-          <Czaty>Czaty</Czaty>
-          <OptionsContainer></OptionsContainer>
-          <NewRoomContainer></NewRoomContainer>
-          <NewMessageContainer></NewMessageContainer>
-        </Nav>
-        <FriendList>
-          {users.map(() => (
-            <FriendListItem>
-              <AvatarContainer>
-                <Avatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-1/p100x100/1908321_487589871394123_3669658637878936630_n.jpg?_nc_cat=102&_nc_sid=7206a8&_nc_ohc=NKZftzVVph4AX-MnHgN&_nc_ht=scontent-frt3-1.xx&tp=6&oh=a21acec7b9417379270463d5f54f872c&oe=5F8D3C5D" />
-              </AvatarContainer>
-              <MessageContainer>
-                <Username>Tomasz Grzyb Martyński </Username>
-                <Message>Rucham ci starego</Message>
-              </MessageContainer>
-            </FriendListItem>
-          ))}
-        </FriendList>
+        <LeftNavContainer>
+          <UserAvatarContainer>
+            <SmallAvatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_ohc=jpVmHYL2xgEAX_eH_Qn&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=e5c68a6969033f02f9d8f25d6a3d5560&oe=5F98D99E" />
+          </UserAvatarContainer>
+          <Chats>Czaty</Chats>
+          <IconsHelper />
+          <LeftNavIconDesktopContainer>
+            <Settings
+              fill="black"
+              height="30px"
+              width="30px"
+              viewBox="0 0 36 36"
+            />
+          </LeftNavIconDesktopContainer>
+          <LeftNavIconDesktopContainer>
+            <CreateNewRoom
+              fill="black"
+              height="28px"
+              width="28px"
+              viewBox="0 0 28 28"
+            />
+          </LeftNavIconDesktopContainer>
+          <LeftNavIconMobileAndDesktopContainer>
+            <Compose
+              fill="black"
+              height="30px"
+              width="30px"
+              viewBox="0 0 36 36"
+            />
+          </LeftNavIconMobileAndDesktopContainer>
+        </LeftNavContainer>
+        <AnotherContainer>
+          <AnotherOneContainer>
+            <AnotherSickContainer>
+              <Div></Div>
+              <SearchBar placeholder="Szukaj w Messengerze"></SearchBar>
+            </AnotherSickContainer>
+          </AnotherOneContainer>
+          <FriendList />
+        </AnotherContainer>
       </LeftContainer>
       <Chat>
-        <MiddleContainer>
-          <path
-            id="Fill-1"
-            d="M18,11.00025 C14.1345,11.00025 11.0005,14.13425 11.0005,18.00025 C11.0005,21.86575 14.1345,24.99975 18,24.99975 C21.8655,24.99975 24.9995,21.86575 24.9995,18.00025 C24.9995,14.13425 21.8655,11.00025 18,11.00025 Z M31,17.42725 L31,18.57325 C31,19.14125 30.679,19.66025 30.171,19.91475 L28.638,20.68075 C28.2975,20.85125 28.0215,21.13725 27.8915,21.49475 C27.7675,21.83575 27.624,22.16775 27.4665,22.49175 C27.3,22.83375 27.286,23.23025 27.406,23.59175 L27.96,25.25225 C28.1395,25.79125 27.999,26.38575 27.5975,26.78725 L26.787,27.59775 C26.3855,27.99925 25.791,28.13975 25.2525,27.95975 L23.595,27.40725 C23.234,27.28725 22.837,27.30125 22.495,27.46775 C22.1705,27.62575 21.837,27.76975 21.4945,27.89425 C21.1355,28.02475 20.849,28.30125 20.6785,28.64275 L19.9145,30.17075 C19.6605,30.67875 19.141,31.00025 18.573,31.00025 L17.427,31.00025 C16.859,31.00025 16.3395,30.67875 16.0855,30.17075 L15.3215,28.64275 C15.151,28.30125 14.864,28.02475 14.5055,27.89425 C14.1625,27.76975 13.83,27.62575 13.505,27.46775 C13.163,27.30125 12.766,27.28725 12.405,27.40725 L10.7475,27.95975 C10.2085,28.13975 9.6145,27.99925 9.213,27.59775 L8.4025,26.78725 C8.0005,26.38575 7.8605,25.79125 8.04,25.25225 L8.594,23.59175 C8.714,23.23025 8.7,22.83375 8.5335,22.49175 C8.376,22.16775 8.2325,21.83575 8.1085,21.49475 C7.9785,21.13725 7.7025,20.85125 7.362,20.68075 L5.829,19.91475 C5.321,19.66025 5,19.14125 5,18.57325 L5,17.42725 C5,16.85875 5.321,16.33925 5.829,16.08525 L7.362,15.31925 C7.7025,15.14875 7.9785,14.86275 8.1085,14.50525 C8.2325,14.16425 8.376,13.83225 8.5335,13.50825 C8.7,13.16625 8.714,12.76975 8.594,12.40825 L8.04,10.74775 C7.8605,10.20875 8.0005,9.61475 8.4025,9.21275 L9.213,8.40275 C9.6145,8.00075 10.2085,7.86025 10.7475,8.04025 L12.405,8.59275 C12.766,8.71275 13.163,8.69875 13.505,8.53225 C13.83,8.37425 14.1625,8.23025 14.5055,8.10575 C14.864,7.97525 15.151,7.69875 15.3215,7.35725 L16.0855,5.82925 C16.3395,5.32125 16.859,5.00025 17.427,5.00025 L18.573,5.00025 C19.141,5.00025 19.6605,5.32125 19.9145,5.82925 L20.6785,7.35725 C20.849,7.69875 21.1355,7.97525 21.4945,8.10575 C21.837,8.23025 22.1705,8.37425 22.495,8.53225 C22.837,8.69875 23.234,8.71275 23.595,8.59275 L25.2525,8.04025 C25.791,7.86025 26.3855,8.00075 26.787,8.40275 L27.5975,9.21275 C27.999,9.61475 28.1395,10.20875 27.96,10.74775 L27.406,12.40825 C27.286,12.76975 27.3,13.16625 27.4665,13.50825 C27.624,13.83225 27.7675,14.16425 27.8915,14.50525 C28.0215,14.86275 28.2975,15.14875 28.638,15.31925 L30.171,16.08525 C30.679,16.33925 31,16.85875 31,17.42725 Z"
-            fill="#000000"
-          ></path>
-        </MiddleContainer>
-        <RightContainer>s</RightContainer>
+        <MiddleNav>
+          <MiddleNavUser>
+            <NextContForAvatar>
+              <SmallAvatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_ohc=jpVmHYL2xgEAX_eH_Qn&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=e5c68a6969033f02f9d8f25d6a3d5560&oe=5F98D99E" />
+            </NextContForAvatar>
+            <Napisy>Adam Galon</Napisy>
+          </MiddleNavUser>
+          <NavList>
+            <NavListItem>
+              <Phone
+                fill="#0099ff"
+                height="32px"
+                width="32px"
+                viewBox="0 0 36 36"
+              />
+            </NavListItem>
+            <NavListItem>
+              <Camcorder
+                fill="#0099ff"
+                height="32px"
+                width="32px"
+                viewBox="0 0 36 36"
+              />
+            </NavListItem>
+            <NavListItem>
+              <SelectedIconDiv />
+              <InfoCircle
+                fill="#0099ff"
+                height="32px"
+                width="32px"
+                viewBox="0 0 36 36"
+              />
+            </NavListItem>
+          </NavList>
+        </MiddleNav>
+        <AnotherKurwaContainer>
+          <MiddleContainer>
+            <AnotherKurwaUserContainer>
+              <AnotherKurwaUserAvatarContainer>
+                <BigAvatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_ohc=jpVmHYL2xgEAX_eH_Qn&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=e5c68a6969033f02f9d8f25d6a3d5560&oe=5F98D99E" />
+              </AnotherKurwaUserAvatarContainer>
+              <NextDiv>Adam Galon</NextDiv>
+            </AnotherKurwaUserContainer>
+          </MiddleContainer>
+          <RightContainer></RightContainer>
+        </AnotherKurwaContainer>
       </Chat>
     </Container>
   );
 };
 
 const Container = styled.div`
-  background-color: #fff;
   display: flex;
-  font-size: 14px;
+  background-color: #fff;
   min-width: 500px;
 `;
 
@@ -62,138 +125,125 @@ const LeftContainer = styled.div`
     max-width: 80px;
   }
 `;
-//
 
-const Nav = styled.div`
-  align-items: center;
-  box-sizing: border-box;
-  display: flex;
-  height: 60px;
-  overflow: hidden;
+const LeftNavContainer = styled.div`
   padding: 10px 16px 10px 16px;
-`;
-const AvaContainer = styled.div`
   display: flex;
-  width: 50px;
-`;
-
-const Ava = styled.img`
-  border-radius: 50%;
-  height: 40px;
-  width: 40px;
-`;
-
-const Czaty = styled.h1`
-  color: rgba(0, 0, 0, 1);
-  font-size: 24px;
-  font-weight: bold;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  transition: all 0.25s;
-  margin: 0px;
-`;
-
-const OptionsContainer = styled.div`
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.04);
-  border-radius: 99px;
-  display: flex;
-  flex: 0 0 auto;
-  justify-content: center;
-  overflow: hidden;
-  transition: height 0.25s, margin-left 0.25s, width 0.25s;
-  height: 36px;
-  margin-left: 12px;
-  width: 36px;
-`;
-
-const NewRoomContainer = styled.div`
-  width: 36px;
-  margin-left: 12px;
-  justify-content: center;
-  height: 36px;
-  display: flex;
-  flex: 0 0 auto;
-  border-bottom-left-radius: 99px;
-  border-bottom-right-radius: 99px;
-  border-top-right-radius: 99px;
-  border-top-left-radius: 99px;
-  background-color: rgba(0, 0, 0, 0.04);
-  align-items: center;
-  cursor: pointer;
-`;
-
-const NewMessageContainer = styled.a`
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.04);
-  border-radius: 99px;
-  display: flex;
-  flex: 0 0 auto;
-  justify-content: center;
-  transition: width 0.25s, height 0.25s;
-  height: 36px;
-  margin-left: 12px;
-  width: 36px;
-`;
-
-//
-
-const FriendList = styled.ul`
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const FriendListItem = styled.li`
-  display: flex;
-  height: 64px;
-  position: relative;
-  background: gray;
-  padding: 0 8px;
-`;
-
-const AvatarContainer = styled.div`
-  margin-right: 12px;
-  display: flex;
-  align-items: center;
-`;
-
-const Avatar = styled.img`
-  height: 50px;
-  width: 50px;
-  border-radius: 50%;
+  box-sizing: border-box;
+  height: 60px;
 
   @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md}, {
-    padding-left: 8px;
-    padding-right: 8px;
+    justify-content: center;
   }
 `;
 
-const MessageContainer = styled.div`
+const UserAvatarContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  flex: 1 1 0px;
-  justify-content: center;
-  padding: 0 16px 0 0;
-  font-weight: 400;
+  height: 40px;
+  margin-right: 12px;
+  width: 40px;
+  cursor: pointer;
+
+  @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md} {
+    display: none;
+  }
+`;
+
+const Chats = styled.h1`
+  margin: 0;
+  font-size: 24px;
+  font-weight: bold;
+  text-overflow: ellipsis;
 
   @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md}, {
     display: none;
   }
 `;
 
-const Username = styled.div`
-  font-size: 15px;
-  color: rgba(0, 0, 0, 1);
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  line-height: 1.4;
+const IconsHelper = styled.div`
+  flex: 1 1 auto;
+
+  @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md}, {
+    display: none;
+  }
 `;
 
-const Message = styled.div`
-  margin-right: 12px;
-  color: rgba(153, 153, 153, 1);
-  font-size: 13px;
+const LeftNavIconDesktopContainer = styled.div`
+  border-radius: 99px;
+  background-color: rgba(0, 0, 0, 0.04);
+  cursor: pointer;
+  height: 36px;
+  width: 36px;
+  margin-left: 12px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md}, {
+    display: none;
+  }
+`;
+
+const LeftNavIconMobileAndDesktopContainer = styled(
+  LeftNavIconDesktopContainer
+)`
+  @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md}, {
+    display: flex;
+    margin: 0px;
+  }
+`;
+
+const AnotherContainer = styled.div`
+  height: 100%;
+`;
+
+const AnotherOneContainer = styled.div`
+  padding: 4px 16px 12px;
+  position: relative;
+
+  @media ${mediaQuery.xs}, ${mediaQuery.sm}, ${mediaQuery.md} {
+    display: none;
+  }
+`;
+
+const AnotherSickContainer = styled.div`
+  position: relative;
+`;
+
+const Div = styled.div`
+  background: red;
+  background-repeat: no-repeat;
+  background-size: 222px 902px;
+  background-position: -144px -794px;
+  content: "";
+  display: inline-block;
+  height: 16px;
+  left: 12px;
+  position: absolute;
+  top: 10px;
+  width: 16px;
+`;
+
+const SearchBar = styled.input`
+  background-color: rgba(0, 0, 0, 0.04);
+  border-radius: 50px;
+  font-size: 15px;
+  height: 36px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-left: 36px;
+  padding-right: 16px;
+  text-align: left;
+  outline: 0;
+  box-sizing: border-box;
+  margin: 0;
+  border-style: none;
+  width: 100%;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+    font-family: Helvetica Neue, Segoe UI, Helvetica, Arial, sans-serif;
+  }
 `;
 
 const MiddleContainer = styled.div`
@@ -201,13 +251,16 @@ const MiddleContainer = styled.div`
   flex: 0 2 33.33%;
   max-width: 420px;
   min-width: 200px;
-  background: brown;
+  box-sizing: border-box;
+  margin: 0 auto;
+  padding-top: 14px;
 `;
 
 const Chat = styled.div`
   display: flex;
   flex: 1 1 0%;
-  flex-direction: row-reverse;
+  flex-direction: column;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
 const RightContainer = styled.div`
@@ -216,5 +269,114 @@ const RightContainer = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  background: red;
+`;
+
+const MiddleNav = styled.div`
+  align-items: center;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+
+  box-sizing: border-box;
+  height: 60px;
+  padding: 8px 8px 8px 16px;
+  position: relative;
+  text-align: center;
+  z-index: 201;
+`;
+
+const MiddleNavUser = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 14px;
+  padding: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: middle;
+  white-space: nowrap;
+`;
+
+const NextContForAvatar = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left: 2px;
+  margin-right: 12px;
+`;
+
+const Napisy = styled.h2`
+  font-size: 15px;
+  font-weight: bold;
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  height: 32px;
+  justify-content: center;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  padding: 9px 0 9px 8px;
+  line-height: 0;
+  list-style-type: none;
+  margin: 0;
+  cursor: pointer;
+`;
+
+const NavListItem = styled.li`
+  display: inline-block;
+  margin-right: 12px;
+
+  :last-child {
+    margin-right: 0px;
+  }
+`;
+
+const SelectedIconDiv = styled.div`
+  background-color: rgb(0, 153, 255);
+  border-radius: 99px;
+  height: 32px;
+  position: absolute;
+  width: 32px;
+  opacity: 0.2;
+`;
+
+const SmallAvatar = styled(Avatar)`
+  height: 40px;
+  width: 40px;
+`;
+
+const BigAvatar = styled(Avatar)`
+  height: 100px;
+  width: 100px;
+`;
+
+const AnotherKurwaContainer = styled.div`
+  display: flex;
+  flex: 1 1 0%;
+  flex-direction: row-reverse;
+`;
+
+const AnotherKurwaUserContainer = styled.div`
+  align-items: center;
+  flex-direction: column;
+  display: flex;
+  padding: 0 14px 16px 14px;
+  word-break: break-word;
+`;
+
+const AnotherKurwaUserAvatarContainer = styled.div`
+  height: 100px;
+  width: 100px;
+`;
+
+const NextDiv = styled.div`
+  align-items: center;
+  flex: 1 1 auto;
+  margin-left: 0;
+  margin-top: 12px;
+  color: #000;
+  display: flex;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
 `;
