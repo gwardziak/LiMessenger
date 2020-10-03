2316dcba8c3ca874338e8e10c233b36796b997ea
jaca <jaca@gmail.com>
2020-10-03 22:02:48 +0200

    add grid to navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -8,95 +8,83 @@ import { Avatar as DefaultAvatar } from "../ui/Avatar";
 export const Navbar = () => {
   return (
     <Container>
-      <UserContainer>
-        <SmallAvatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.30497-1/c29.0.100.100a/p100x100/84241059_189132118950875_4138507100605120512_n.jpg?_nc_cat=1&_nc_sid=7206a8&_nc_ohc=jpVmHYL2xgEAX_eH_Qn&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=e5c68a6969033f02f9d8f25d6a3d5560&oe=5F98D99E" />
-        <Username>Adam Galon</Username>
-      </UserContainer>
-      <IconList>
-        <Icon>
-          <Phone
-            fill="#0099ff"
-            height="32px"
-            width="32px"
-            viewBox="0 0 36 36"
-          />
-        </Icon>
-        <Icon>
-          <Camcorder
-            fill="#0099ff"
-            height="32px"
-            width="32px"
-            viewBox="0 0 36 36"
-          />
-        </Icon>
-        <Icon>
-          <SelectedIconHelper />
-          <InfoCircle
-            fill="#0099ff"
-            height="32px"
-            width="32px"
-            viewBox="0 0 36 36"
-          />
-        </Icon>
-      </IconList>
+      <Avatar src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-1/cp0/p60x60/46844435_1931467286943369_1021085731109470208_o.jpg?_nc_cat=109&_nc_sid=dbb9e7&_nc_ohc=4pMEq64VPgIAX9y4pTX&_nc_ht=scontent-frt3-1.xx&_nc_tp=27&oh=8c85ae9698f070ec16a84beef5df483e&oe=5F9D606C" />
+      <Username>Filip zawada</Username>
+      <Acticity>Aktywny(a) przez: Messenger</Acticity>
+      <PhoneIcon viewBox="0 0 36 36" />
+      <CamcorderIcon viewBox="0 0 36 36" />
+      <InfoIcon viewBox="0 0 36 36" />
+      <IconHelper />
     </Container>
   );
 };
 
 const Container = styled.div`
   grid-area: nav;
-  display: flex;
+  display: grid;
+  grid-template-columns: 40px 1fr 32px 32px 32px;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas: "avatar username  phoneIcon camcorderIcon infoIcon" "avatar activity phoneIcon camcorderIcon infoIcon";
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
-  justify-content: space-between;
   box-sizing: border-box;
   padding: 8px 8px 8px 16px;
-`;
-
-const UserContainer = styled.div`
-  display: flex;
+  grid-column-gap: 12px;
   align-items: center;
-  font-size: 14px;
-  text-overflow: ellipsis;
-  white-space: nowrap;
 `;
 
-const SmallAvatar = styled(DefaultAvatar)`
+const Avatar = styled(DefaultAvatar)`
+  grid-area: avatar;
   height: 40px;
   width: 40px;
-  margin-left: 2px;
-  margin-right: 12px;
 `;
 
 const Username = styled.h2`
+  grid-area: username;
+  margin: 0;
   font-size: 15px;
   font-weight: bold;
-  display: flex;
-  flex-direction: column;
-  height: 32px;
-  justify-content: center;
 `;
 
-const IconList = styled.ul`
-  display: flex;
-  padding: 9px 0 9px 8px;
-  line-height: 0;
+const Acticity = styled.div`
+  grid-area: activity;
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.4);
+  align-self: stretch;
+`;
+
+const PhoneIcon = styled(Phone)`
+  grid-area: phoneIcon;
   cursor: pointer;
-  align-items: center;
+  padding: 9px 0 9px 8px;
+  fill: #0099ff;
+  height: 32px;
+  width: 32px;
 `;
 
-const Icon = styled.li`
-  display: inline-block;
-  margin-right: 12px;
-  :last-child {
-    margin-right: 0px;
-  }
+const CamcorderIcon = styled(Camcorder)`
+  grid-area: camcorderIcon;
+  cursor: pointer;
+  padding: 9px 0 9px 8px;
+  fill: #0099ff;
+  height: 32px;
+  width: 32px;
 `;
 
-const SelectedIconHelper = styled.div`
+const InfoIcon = styled(InfoCircle)`
+  grid-area: infoIcon;
+  cursor: pointer;
+  fill: #0099ff;
+  height: 32px;
+  width: 32px;
+  padding: 9px 0 9px 8px;
+`;
+
+const IconHelper = styled.div`
+  grid-area: infoIcon;
   background-color: rgb(0, 153, 255);
   border-radius: 99px;
   height: 32px;
-  position: absolute;
   width: 32px;
   opacity: 0.2;
+  margin-left: 8px;
 `;
